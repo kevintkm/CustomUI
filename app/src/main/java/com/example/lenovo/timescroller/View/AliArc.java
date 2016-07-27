80f6d8cad7a017922341e2034129764e93c741f6
keming.tian <keming.tian@weimob.com>
2016-07-27 14:31:49 +0800

    【AliArc】改变基类

--- app/src/main/java/com/example/lenovo/timescroller/View/AliArc.java
+++ app/src/main/java/com/example/lenovo/timescroller/View/AliArc.java
@@ -15,6 +15,7 @@ import android.graphics.Shader;
 import android.graphics.SweepGradient;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import com.example.lenovo.timescroller.R;
 
@@ -27,7 +28,7 @@ import com.example.lenovo.timescroller.R;
  * 图层绘制的一些概念
  * 脏矩形技术
  */
-public class AliArc extends View {
+public class AliArc extends FrameLayout {
 
     private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private final int[] mColors = new int[]{0xffff0000, 0xffffff00, 0xff00ff00,
@@ -37,17 +38,17 @@ public class AliArc extends View {
     private int CENTERX;
     private int width;
     private int height;
-    Bitmap bitmapLocation ;
-    int  bitmapWidth;
+    Bitmap bitmapLocation;
+    int bitmapWidth;
     int bitmapHeight;
-    private float totalRotateAngle = 0f ;
-    private float currentRotateAngle = 0f ;
+    private float totalRotateAngle = 0f;
+    private float currentRotateAngle = 0f;
     /**
      * 单位时间内旋转角度
      */
     private float rotateAngle = 0f;
 
-    private boolean isSetReferValue = false ;
+    private boolean isSetReferValue = false;
 
 
     public AliArc(Context context) {
@@ -62,9 +63,11 @@ public class AliArc extends View {
         super(context, attrs, defStyleAttr);
         init();
     }
+
     public static interface RotateListener {
-        public void rotate(float sweepAngle,float value);
+        public void rotate(float sweepAngle, float value);
     }
+
     private void init() {
         mPaint.setShader(mShader);
         //mPaint.setColor(0xffff0000);
@@ -94,14 +97,12 @@ public class AliArc extends View {
         innerPaint.setStyle(Paint.Style.STROKE);
         innerPaint.setStrokeWidth(4);
         innerPaint.setColor(Color.GRAY);
-        PathEffect effect = new DashPathEffect(new float[]{15, 15, 15, 15}, 0);
+        PathEffect effect = new DashPathEffect(new float[]{5, 5, 5}, 0);
         Paint vitualPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         vitualPaint.setStyle(Paint.Style.STROKE);
         vitualPaint.setStrokeWidth(4);
         vitualPaint.setColor(Color.GREEN);
         vitualPaint.setPathEffect(effect);
-
-        canvas.save();
         canvas.translate(CENTERX, CENTERX);
         canvas.drawCircle(0, 0, r * 5 / 8, vitualPaint);
         canvas.drawCircle(0, 0, r * 3 / 4, innerPaint);
@@ -112,7 +113,7 @@ public class AliArc extends View {
         bigPaint.setColor(Color.WHITE);
         bigPaint.setStrokeWidth(4);
         Paint smallPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        smallPaint.setColor(Color.YELLOW);
+        smallPaint.setColor(Color.WHITE);
         smallPaint.setStrokeWidth(2);
         //每次画最右边直线，通过旋转实现分割
         int startX = (int) (width - mPaint.getStrokeWidth());
@@ -138,15 +139,15 @@ public class AliArc extends View {
         path.close();
         canvas.drawPath(path, oval);
 
-        String[] text = {"950","极好","700","优秀","650","良好","600","中等","550","较差","350","很差","150"};
+        String[] text = {"950", "极好", "700", "优秀", "650", "良好", "600", "中等", "550", "较差", "350", "很差", "150"};
 
         Paint textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         textPaint.setColor(Color.RED);
         textPaint.setTextSize(18);
-        for ( int i=0;i<=12;i++) {
+        for (int i = 0; i <= 12; i++) {
             canvas.save();
-            canvas.rotate(-(-120 + 20 * i ), CENTERX, CENTERX);
-            canvas.drawText(text[i],CENTERX - 20 ,CENTERX * 3 / 16,textPaint);
+            canvas.rotate(-(-120 + 20 * i), CENTERX, CENTERX);
+            canvas.drawText(text[i], CENTERX - 20, CENTERX * 3 / 16, textPaint);
             canvas.restore();
         }
 
@@ -161,39 +162,39 @@ public class AliArc extends View {
         bitmapWidth = bitmapLocation.getWidth();
         bitmapHeight = bitmapLocation.getHeight();
 
-        if (isSetReferValue){
-            float r1 =r*3/4;
+        if (isSetReferValue) {
+            float r1 = r * 3 / 4;
             canvas.save();
             canvas.translate(CENTERX, CENTERX);
             canvas.drawArc(new RectF(-r1, -r1, r1, r1), -210, currentRotateAngle, false, paintMiddleArc);
             canvas.rotate(-30 + currentRotateAngle);
             Matrix matrix = new Matrix();
-            matrix.preTranslate(-r1-bitmapWidth*3/8,-bitmapHeight/2);
+            matrix.preTranslate(-r1 - bitmapWidth * 3/ 8, -bitmapHeight / 2);
             canvas.drawBitmap(bitmapLocation, matrix, paintBitmap);
             canvas.restore();
         }
 
     }
 
-    public void setValue (int referValue,final RotateListener rotateListener){
-        isSetReferValue = !isSetReferValue ;
-        if ( referValue <= 150 ) {
-            totalRotateAngle = 0f ;
-        } else if ( referValue <= 550 ) {
-            totalRotateAngle = ( referValue - 150 ) * 80 / 400f ;
-        } else if ( referValue <= 700 ) {
-            totalRotateAngle = ( referValue - 550 ) * 120 / 150f + 80 ;
-        } else if ( referValue <= 950 ) {
-            totalRotateAngle = ( referValue - 700 ) * 40 / 250f + 200;
+    public void setValue(int referValue, final RotateListener rotateListener) {
+        isSetReferValue = !isSetReferValue;
+        if (referValue <= 150) {
+            totalRotateAngle = 0f;
+        } else if (referValue <= 550) {
+            totalRotateAngle = (referValue - 150) * 80 / 400f;
+        } else if (referValue <= 700) {
+            totalRotateAngle = (referValue - 550) * 120 / 150f + 80;
+        } else if (referValue <= 950) {
+            totalRotateAngle = (referValue - 700) * 40 / 250f + 200;
         } else {
-            totalRotateAngle = 210f ;
+            totalRotateAngle = 210f;
         }
 
-        rotateAngle=totalRotateAngle/60;
+        rotateAngle = totalRotateAngle / 60;
         new Thread(new Runnable() {
             @Override
             public void run() {
-                boolean rotating = true ;
+                boolean rotating = true;
                 float value = 350;
                 while (rotating) {
                     try {
@@ -202,19 +203,19 @@ public class AliArc extends View {
                         e.printStackTrace();
                     }
                     currentRotateAngle += rotateAngle;
-                    if ( currentRotateAngle >= totalRotateAngle ) {
+                    if (currentRotateAngle >= totalRotateAngle) {
                         currentRotateAngle = totalRotateAngle;
                         rotating = false;
                     }
-                    if ( null != rotateListener) {
-                        if ( currentRotateAngle <= 80 ) {
-                            value = 350 + ( currentRotateAngle / 80 ) * 400 ;
-                        } else if ( currentRotateAngle <= 200 ) {
-                            value = 550 + ( ( currentRotateAngle  - 80 )/ 120 ) * 150 ;
+                    if (null != rotateListener) {
+                        if (currentRotateAngle <= 80) {
+                            value = 350 + (currentRotateAngle / 80) * 400;
+                        } else if (currentRotateAngle <= 200) {
+                            value = 550 + ((currentRotateAngle - 80) / 120) * 150;
                         } else {
-                            value = 700 + ( ( currentRotateAngle - 200 ) / 40 ) * 250 ;
+                            value = 700 + ((currentRotateAngle - 200) / 40) * 250;
                         }
-                        rotateListener.rotate(currentRotateAngle,value);
+                        rotateListener.rotate(currentRotateAngle, value);
                     }
                     postInvalidate();
                 }
