3d3f748496e5d59160ce838bab8cb7a8d0c499cc
keming.tian <keming.tian@weimob.com>
2016-08-30 19:15:30 +0800

    【模块学习】拖拽

--- app/src/main/java/com/example/lenovo/timescroller/Fragment/SourceAnalyseFragment.java
+++ app/src/main/java/com/example/lenovo/timescroller/Fragment/SourceAnalyseFragment.java
@@ -7,10 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.lenovo.timescroller.R;
+import com.example.lenovo.timescroller.Util.Util;
 import com.example.lenovo.timescroller.View.DragViewGroup;
 import com.example.lenovo.timescroller.View.SimpleScroller;
 
@@ -31,9 +33,7 @@ public class SourceAnalyseFragment extends Fragment{
         //TODO
         /**
          * 多次循环向四周拖动，viewGroup会越来越小
-         */
         DragViewGroup viewGroup = new DragViewGroup(getContext());
-        viewGroup.setPadding(50,50,50,50);
         viewGroup.setBackgroundColor(getResources().getColor(R.color.A9A9A9));
         Button textView = new Button(getContext());
         textView.setPadding(120,120,120,120);
@@ -41,6 +41,22 @@ public class SourceAnalyseFragment extends Fragment{
         textView.setBackgroundColor(getResources().getColor(R.color.blue));
         viewGroup.addView(textView);
         return viewGroup;
+         */
+
+        View view = inflater.inflate(R.layout.fragment_draglinear_layout,container,false);
+        DragViewGroup group = (DragViewGroup) view;
+        for (int i = 0; i < 5; i++) {
+            ImageView image = new ImageView(getContext());
+            image.setBackgroundResource(R.drawable.kevin);
+            int screenWidth = Util.getScreenWidth(getActivity());
+            int width = (screenWidth-12)/5;
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width,Util.dpToPx(getResources(),78));
+            params.rightMargin = Util.dpToPx(getResources(),3);
+            image.setOnClickListener(null);
+            group.addView(image,params);
+        }
+        return view;
+
     }
 
 }
--- app/src/main/java/com/example/lenovo/timescroller/View/DragViewGroup.java
+++ app/src/main/java/com/example/lenovo/timescroller/View/DragViewGroup.java
@@ -3,8 +3,15 @@ package com.example.lenovo.timescroller.View;
 import android.content.Context;
 import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Scroller;
+
+import com.example.lenovo.timescroller.Util.Util;
+
+import java.util.ArrayList;
 
 /**
  * 自定义拖拽View
@@ -18,10 +25,11 @@ public class DragViewGroup extends LinearLayout {
 
     private float mLastX;
     private float mLastY;
+    private Scroller mScroller;
 
     // The ‘active pointer’ is the one currently moving our object.
-    private int INVALID_POINTER_ID = -1000;
-    private int mActivePointerId = INVALID_POINTER_ID;
+//    private int INVALID_POINTER_ID = -1000;
+//    private int mActivePointerId = INVALID_POINTER_ID;
 
 
     public DragViewGroup(Context context) {
@@ -34,88 +42,93 @@ public class DragViewGroup extends LinearLayout {
 
     public DragViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mScroller = new Scroller(context);
         init();
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-
-        int action = MotionEventCompat.getActionMasked(event);
-        switch (action) {
-            case MotionEvent.ACTION_DOWN:
-//                mLastX = event.getX();
-//                mLastY = event.getY();
-                break;
-            case MotionEvent.ACTION_MOVE:
-
-                final int pointerIndex =
-                        MotionEventCompat.findPointerIndex(event, mActivePointerId);
-
-                final float x = MotionEventCompat.getX(event, pointerIndex);
-                final float y = MotionEventCompat.getY(event, pointerIndex);
-
-                float currentX = x;
-                float currentY = y;
-                float dx = currentX - mLastX;
-                float dy = currentY - mLastY;
-                layout((int) (getLeft() + dx), (int) (getTop() + dy), (int) (getRight() + dx), (int) (getBottom() + dy));
-                invalidate();
-                break;
-            case MotionEvent.ACTION_UP: {
-                mActivePointerId = INVALID_POINTER_ID;
-                break;
-            }
-
-            case MotionEvent.ACTION_CANCEL: {
-                mActivePointerId = INVALID_POINTER_ID;
-                break;
-            }
-            case MotionEvent.ACTION_POINTER_UP: {
-
-                int pointerInd = MotionEventCompat.getActionIndex(event);
-                final int pointerId = MotionEventCompat.getPointerId(event, pointerInd);
-
-                if (pointerId == mActivePointerId) {
-                    // This was our active pointer going up. Choose a new
-                    // active pointer and adjust accordingly.
-                    final int newPointerIndex = pointerInd == 0 ? 1 : 0;
-                    mLastX = MotionEventCompat.getX(event, newPointerIndex);
-                    mLastY = MotionEventCompat.getY(event, newPointerIndex);
-                    mActivePointerId = MotionEventCompat.getPointerId(event, newPointerIndex);
-                }
-                break;
-            }
+    public void computeScroll() {
+        super.computeScroll();
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            invalidate();
         }
-
-
-        return true;
     }
+
+    /**
+     * 通过遍历RootView树得到当前点击范围内的子view
+     *
+     * @param event
+     * @return
+     */
+
     @Override
-    public boolean onInterceptTouchEvent(MotionEvent event) {
+    public boolean dispatchTouchEvent(MotionEvent event) {
         int action = event.getAction() & MotionEvent.ACTION_MASK;
         //为了使手指按在Button等可点击的控件上任可以滑动，需要拦截滑动实践
         //并且为了使坐标准确，在此处记录按下的点
         switch (action) {
             case MotionEvent.ACTION_MOVE:
-                return true;
+//                final int pointer =
+//                        MotionEventCompat.findPointerIndex(event, mActivePointerId);
+
+                final float x1 = event.getRawX();
+                final float y1 = event.getRawY();
+                float currentX = x1;
+                float currentY = y1;
+                float dx = currentX - mLastX;
+                float dy = currentY - mLastY;
+                View child = getTouchTarget(this, (int) currentX, (int) currentY);
+                scrollTo((int) -dx, 0);
+                break;
             case MotionEvent.ACTION_DOWN:
 
-                final int pointerIndex = MotionEventCompat.getActionIndex(event);
-                mActivePointerId=MotionEventCompat.getPointerId(event,pointerIndex);
+            /*    final int pointerIndex = MotionEventCompat.getActionIndex(event);
+                mActivePointerId=MotionEventCompat.getPointerId(event,pointerIndex);*/
 
-                final float x = MotionEventCompat.getX(event, pointerIndex);
-                final float y = MotionEventCompat.getY(event, pointerIndex);
-
+//                final float x = MotionEventCompat.getX(event, pointerIndex);
+//                final float y = MotionEventCompat.getY(event, pointerIndex);
+                float x = event.getRawX();
+                float y = event.getRawY();
+                Log.d("-----", x + "");
+                Log.d("-----", y + "");
                 mLastX = x;
-                mLastY= y;
+                mLastY = y;
                 // Save the ID of this pointer (for dragging)
-                mActivePointerId = MotionEventCompat.getPointerId(event, 0);
-                return false;
+                //  mActivePointerId = MotionEventCompat.getPointerId(event, 0);
+                break;
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+    private View getTouchTarget(View view, int x, int y) {
+        View target = null;
+        ArrayList<View> TouchableViews = view.getTouchables();
+        for (View child : TouchableViews) {
+            if (isTouchPointInView(child, x, y)) {
+                target = child;
+                break;
+            }
+        }
+
+        return target;
+    }
+
+    private boolean isTouchPointInView(View view, int x, int y) {
+        int[] location = new int[2];
+        view.getLocationOnScreen(location);
+        int left = location[0];
+        int top = location[1];
+        int right = left + view.getMeasuredWidth();
+        int bottom = top + view.getMeasuredHeight();
+        if (view.isClickable() && y >= top && y <= bottom
+                && x >= left && x <= right) {
+            return true;
         }
         return false;
     }
+
     private void init() {
     }
-
 
 }
