9e36e9f046d000d220230d125dc1e272aafa0de3
keming.tian <keming.tian@weimob.com>
2016-08-31 18:56:51 +0800

    【常用控件】拖拽滑动排序完成

--- app/src/main/java/com/example/lenovo/timescroller/View/DragViewGroup.java
+++ app/src/main/java/com/example/lenovo/timescroller/View/DragViewGroup.java
@@ -1,17 +1,22 @@
 package com.example.lenovo.timescroller.View;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
-import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Scroller;
 
+import com.example.lenovo.timescroller.R;
 import com.example.lenovo.timescroller.Util.Util;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * 自定义拖拽View
@@ -25,7 +30,11 @@ public class DragViewGroup extends LinearLayout {
 
     private float mLastX;
     private float mLastY;
+    int targetInedex;
+    int currentIndex;
+    int childCount;
     private Scroller mScroller;
+    private ImageView currentView;
 
     // The ‘active pointer’ is the one currently moving our object.
 //    private int INVALID_POINTER_ID = -1000;
@@ -78,8 +87,12 @@ public class DragViewGroup extends LinearLayout {
                 float currentY = y1;
                 float dx = currentX - mLastX;
                 float dy = currentY - mLastY;
-                View child = getTouchTarget(this, (int) currentX, (int) currentY);
-                scrollTo((int) -dx, 0);
+                currentView.setTranslationX(dx);
+//                ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(currentView, "translationX", dx);
+//                objectAnimator.setDuration(0);
+//                objectAnimator.start();
+                onDrag(dx);
+                // scrollTo((int) -dx, 0);
                 break;
             case MotionEvent.ACTION_DOWN:
 
@@ -92,18 +105,67 @@ public class DragViewGroup extends LinearLayout {
                 float y = event.getRawY();
                 Log.d("-----", x + "");
                 Log.d("-----", y + "");
+                currentView = (ImageView) getTouchTarget(this, (int) x, (int) y);
                 mLastX = x;
                 mLastY = y;
                 // Save the ID of this pointer (for dragging)
                 //  mActivePointerId = MotionEventCompat.getPointerId(event, 0);
                 break;
+            case MotionEvent.ACTION_UP:
+                for (int i = 0; i < getChildCount(); i++) {
+                    View child = getChildAt(i);
+                    child.setTranslationX(0);
+                }
+                removeView(currentView);
+                addView(currentView,currentIndex+targetInedex);
+                break;
         }
         return super.dispatchTouchEvent(event);
+    }
+
+    private HashMap<View,ObjectAnimator> viewObjectAnimatorHashMap = new HashMap<>();
+
+    private void onDrag(float dx) {
+        currentIndex = indexOfChild(currentView);
+        int offset = (int) dx / (currentView.getMeasuredWidth() / 2 + Util.dpToPx(getResources(), 3));
+        float ss = -(Util.dpToPx(getResources(), 3) + currentView.getWidth());
+        if (offset > 0) {
+            targetInedex = (offset + 1) / 2;
+            for (int i = 1; i < targetInedex + 1; i++) {
+
+               View child = getChildAt(currentIndex + i);
+
+                child.setTranslationX(ss);
+                 /*if(child == currentView){
+                    continue;
+                }
+                float ss = -(Util.dpToPx(getResources(), 3) + currentView.getWidth());
+                if(child.getTranslationX() == ss){
+                    continue;
+                }
+//                child.setTranslationX(ss);
+                ObjectAnimator objectAnimator = viewObjectAnimatorHashMap.get(child);
+                if(objectAnimator==null || !objectAnimator.isRunning()){
+                    objectAnimator = ObjectAnimator.ofFloat(child, "translationX", ss);
+                    viewObjectAnimatorHashMap.put(child, objectAnimator);
+                    objectAnimator.setDuration(100);
+                    objectAnimator.start();
+                }*/
+            }
+        }else if (offset<0){
+            targetInedex = (offset - 1) / 2;
+            for (int i = -1; i > targetInedex -1 ; i--) {
+                View child = getChildAt(currentIndex + i);
+
+                child.setTranslationX(-ss);
+            }
+        }
     }
 
     private View getTouchTarget(View view, int x, int y) {
         View target = null;
         ArrayList<View> TouchableViews = view.getTouchables();
+        childCount = TouchableViews.size();
         for (View child : TouchableViews) {
             if (isTouchPointInView(child, x, y)) {
                 target = child;
