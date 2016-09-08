973ee68e8e9deb93d37440e54d5f1624652a96de
keming.tian <keming.tian@weimob.com>
2016-09-08 15:03:43 +0800

    【】

--- app/src/main/java/com/example/lenovo/timescroller/Service/AlarmManagers.java
+++ app/src/main/java/com/example/lenovo/timescroller/Service/AlarmManagers.java
@@ -1,5 +1,7 @@
 package com.example.lenovo.timescroller.service;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
@@ -12,19 +14,18 @@ public class AlarmManagers {
     public static void register(Context context) {
         Calendar today = Calendar.getInstance();
         Calendar now = Calendar.getInstance();
-        today.set(Calendar.HOUR_OF_DAY,18);
+        today.set(Calendar.HOUR_OF_DAY,11);
         today.set(Calendar.MINUTE,22);
         today.set(Calendar.SECOND,48);
-//        if (now.after(today)){
-//            return;
-//        }
+        if (now.after(today)){
+            return;
+        }
 
         Intent intent = new Intent("com.example.lenovo.timescroller");
         intent.setClass(context, AlarmReciver.class);
-        context.sendBroadcast(intent);
-//        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,235,intent,PendingIntent.FLAG_UPDATE_CURRENT);
-//        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-//        alarmManager.set(AlarmManager.RTC_WAKEUP,today.getTimeInMillis(),pendingIntent);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,235,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP,today.getTimeInMillis(),pendingIntent);
 
 
     }
